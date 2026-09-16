d253ba4bcf9780668139e6cda62b26c802a005b3
d3matan-debug <d3matan@gmail.com>
2026-09-16 12:39:23 +0300

    Update print statement from 'Hello' to 'Goodbye'

--- bot.py
+++ bot.py
@@ -10,7 +10,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Bot is running!"
+    return "Bot is running!", 200
 
 def run():
  app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 10000)))
